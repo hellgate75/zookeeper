680c62bf9a79a09924dc124f8441f4f8cd44a084
hellgate75 <hellgate75@gmail.com>
2017-08-04 03:59:17 +0100

    Improvement of Zookeeper configuration

--- start-zookeeper.sh
+++ start-zookeeper.sh
@@ -7,30 +7,46 @@ if ! [[ -z "$RUNNING" ]]; then
   exit 0
 fi
 
-function download_files() {
+function download_file() {
   if [[ -z "$(echo $2|grep -i 'https://')" ]]; then
     curl -L -o $1 $2
+    return "$?"
   else
     curl -sSL -o $1 $2
+    return "$?"
+  fi
+}
+
+function download_file() {
+  if [[ -z "$(echo $2|grep -i 'https://')" ]]; then
+    curl -L -o $1 $2
+    return "$?"
+  else
+    curl -sSL -o $1 $2
+    return "$?"
   fi
 }
 
 if ! [[ -z "$ZOOKEEPER_CONFIGURATION_URL" ]]; then
-  echo "Downloading ZooKeeper configuration from url : $ZOOKEEPER_CONFIGURATION_URL ..."
+  echo "Downloading Apache ZooKeeper configuration from url : $ZOOKEEPER_CONFIGURATION_URL ..."
   download_files /root/zoo-dwld.cfg $ZOOKEEPER_CONFIGURATION_URL
   if [[ -e /root/zoo-dwld.cfg ]]; then
-    echo "Applying ZooKeeper download configuration ..."
+    echo "Applying Apache ZooKeeper download configuration ..."
     cp /root/zoo-dwld.cfg $ZK_HOME/conf/zoo.cfg
     rm -f /root/zoo-dwld.cfg
+  else
+    echo "Problems downloading Apache ZooKeeper configuration from url : $ZOOKEEPER_CONFIGURATION_URL ..."
   fi
 else
  if ! [[ -z "$ZOOKEEPER_CONFIGURATION_SCRIPT_URL" ]]; then
-   echo "Downloading ZooKeeper configuration from url : $ZOOKEEPER_CONFIGURATION_SCRIPT_URL ..."
+   echo "Downloading Apache ZooKeeper configuration shell script from url : $ZOOKEEPER_CONFIGURATION_SCRIPT_URL ..."
    download_files /root/setup-zookeeper.sh $ZOOKEEPER_CONFIGURATION_SCRIPT_URL
    if [[ -e /root/setup-zookeeper.sh ]]; then
-     echo "Applying ZooKeeper downloaded script configuration ..."
+     echo "Applying Apache ZooKeeper downloaded shell script configuration ..."
      . /root/setup-zookeeper.sh
      rm -f /root/setup-zookeeper.sh
+   else
+     echo "Problems downloading Apache ZooKeeper configuration shell script from url : $ZOOKEEPER_CONFIGURATION_SCRIPT_URL ..."
    fi
  fi
    configure-zookeeper
